4de579a2acdcb6fb6080ce6d54b7c6661a93a574
su <abcc0785@gmail.com>
2022-03-15 12:54:15 +0900

    비밀번호 암호화 기능 구현 완료

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,26 +11,25 @@ module.exports = {
           .json({ message: "이메일, 비밀번호, 닉네임 필수 입력" });
       }
 
-
       models.signup.findUser(email, (error, result) => {
         console.log("유저찾기")
         if (error) {
           return res.status(409).json({ message: "이메일이 이미 있습니다." });
         }else{
-          return models.signup.post(email, password, nickname, (error, result) => {
-            console.log("진짜 가입")
-            if (error) {
-              return res.status(409).json("message : signup error");
-            } else {
-              return res.status(201).json(result);
-            }
+          // 비밀번호 해쉬화 한 후 회원가입 할 때 그 비밀번호를 넣도록
+          bcrypt.hash(password,10,(error,hash) => {
+            if(error) return res.status(500).json({ message: "비번암호화실패"})
+            return models.signup.post(email, hash, nickname, (error, result) => {
+              console.log("진짜 가입")
+              if (error) {
+                return res.status(409).json({message : "signup error"});
+              } else {
+                return res.status(201).json(result);
+              }
+            })
           })
         }
       });
-
-      // 비밀번호 해쉬화 한 후 회원가입 할 때 그 비밀번호를 넣도록
-
-
     },
   },
 };
