8a3c13e1e003fb3945cd08c46c5553e7cf7f2fe4
Jaehyeon <parkjh9370@naver.com>
2022-03-16 21:05:31 +0900

    ~회원가입 구현

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+
+const { sequelize } = require('./models');
+sequelize.sync({ force : false })
+    .then(() => {
+        console.log('데이터베이스 연결 성공')
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+
 const app = express();
-
-
 
 app.use(cors());
 app.use(helmet());
@@ -14,8 +22,6 @@ app.use(express.json());
 const indexRouter = require('./routes/index')
 app.use('/', indexRouter);
 
-
-// const 
 app.listen(3000, () => {
     console.log(`3000번 포트에서 대기중`)
 })
---
+++ models/user.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize');
+ 
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      email: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      }, 
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      nickname: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: true,
+      modelName: 'User',
+      tableName: 'users',
+      paranoid: false,
+      // mb4 -> 이모티콘도 사용 가능 
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    });
+  }
+
+  static associate(db) {
+   
+  }
+}; 
