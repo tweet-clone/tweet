f77a85bce00c079fca4110a5c981cd5d1d826425
su <abcc0785@gmail.com>
2022-03-24 12:58:47 +0900

    tweet 에러 처리 완료

--- controllers/tweet.js
+++ controllers/tweet.js
@@ -5,8 +5,12 @@ module.exports = {
 
     //테이블의 모든 트윗 불러오기
     get: async (req,res) => {
-      const result = await tweetmodel.get()
-      res.status(200).json(result)
+      try{
+        const result = await tweetmodel.get()
+        res.status(200).json(result)
+      }catch(err){
+        res.sendStatus(500)
+      }
     },
     //게시글 작성
     post: async (req,res) => {
@@ -17,10 +21,14 @@ module.exports = {
 
       if(!content ) return res.status(400).json({message: "content must exist" })
       //picture는 사용자 단에서 전달할 수도 있고 전달하지 않을 수도 있다. 
+      try{
+        const tweetId = await tweetmodel.post(id,content,picture)
+        //tweet 테이블에 사용자id, 게시글 내용, (사진) 추가하기: model 
+        return res.status(201).json({ data: tweetId, message : "ok"})
+      }catch(err){
+        res.sendStatus(500)
+      }
 
-      const tweetId = await tweetmodel.post(id,content,picture)
-      //tweet 테이블에 사용자id, 게시글 내용, (사진) 추가하기: model 
-      return res.status(201).json({ data: tweetId, message : "ok"})
     },
     //게시글 수정
     put: async (req,res) => {
@@ -32,9 +40,14 @@ module.exports = {
 
       if(!userId || !tweetId || !content ) return res.status(400).json({ message: "userId, tweetId, content must exist" })
       
-      const modifiedTweet = await tweetmodel.put(content, picture, tweetId, userId)
-      console.log(modifiedTweet)
-      return res.status(204).json({ data: modifiedTweet, message : "ok"})
+      try{
+        const modifiedTweet = await tweetmodel.put(content, picture, tweetId, userId)
+        console.log(modifiedTweet)
+        return res.status(200).json({ data: modifiedTweet, message : "ok"})
+      }catch(err){
+        res.sendStatus(500)
+      }
+
     },
     //게시글 삭제
     delete: async (req,res) => {
@@ -42,10 +55,13 @@ module.exports = {
       const { id: tweetId } = req.params
       
       if(!userId || !tweetId) return res.status(400).json({ message: "userId, tweetId must exist"})
-
-      const result = await tweetmodel.delete(tweetId , userId)
+      try{
+        const result = await tweetmodel.delete(tweetId , userId)
       console.log(result)
       return res.status(201).json({ result, message : "ok"})
+      }catch(err){
+        res.sendStatus(500)
+      }
     }
 }
 
